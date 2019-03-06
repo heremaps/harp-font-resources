ed30f62071731ec5329dda588f587fa758a70462
Ignacio Julve <41909512+musculman@users.noreply.github.com>
2019-03-06 14:50:28 +0100

    Added Apache-2.0 license header to source code
    Signed-off-by: Ignacio Julve <41909512+musculman@users.noreply.github.com>

--- scripts/create-font-catalog.ts
+++ scripts/create-font-catalog.ts
@@ -1,3 +1,8 @@
+ /**
+  * Copyright (C) 2018-2019 HERE Europe B.V.
+  * Licensed under Apache 2.0, see full license in LICENSE
+  * SPDX-License-Identifier: Apache-2.0
+  */
 /**
  * Script designed to generate the TextCanvas' FontCatalog assets.
  * Usage: yarn create-font-catalog -- -i <inputFile> -o <outputDir>
